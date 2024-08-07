5022d0abe8d26845f205f791b316f8c237402e58
Muhammad Zeeshan <35112049+itszeeshan@users.noreply.github.com>
2024-08-08 00:45:37 +0500

    feat: solution to fizz buzz Problem

---
+++ fizzBuzzProblem.ts
@@ -0,0 +1,8 @@
+(function fizzBuzz(): void {
+    for (let i = 1; i <= 100; i++) {
+        let output = '';
+        if (i % 3 === 0) output += 'Fizz';
+        if (i % 5 === 0) output += 'Buzz';
+        console.log(output || i);
+    }
+})(); //Output: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, ...
