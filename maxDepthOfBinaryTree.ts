03bc3c5a76b394a472048b53be2669cfbbd7bce5
Muhammad Zeeshan <35112049+itszeeshan@users.noreply.github.com>
2024-08-14 01:41:20 +0500

    feat: solution to Maximum Depth of Binary Tree Problem

---
+++ maxDepthOfBinaryTree.ts
@@ -0,0 +1,27 @@
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+function maxDepth(root: TreeNode | null): number {
+    if (root === null) {
+        return 0;
+    }
+
+    const leftDepth = maxDepth(root.left);
+    const rightDepth = maxDepth(root.right);
+
+    return Math.max(leftDepth, rightDepth) + 1;
+}
+
+const root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+
+console.log(maxDepth(root)); // Output: 3
